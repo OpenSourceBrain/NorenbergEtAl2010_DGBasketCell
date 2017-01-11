4040273e89218d267ba8913d6e5b2671cf707d0f
Padraig Gleeson <p.gleeson@gmail.com>
2017-01-11 11:20:36 +0000

    Test export of another cell

--- NeuroML2/export_nml2.py
+++ NeuroML2/export_nml2.py
@@ -1,7 +1,6 @@
 from pyneuroml.neuron import export_to_neuroml2
 
-import sys
-import os
 
 
-export_to_neuroml2("load_bc6.hoc", "BC6.cell.nml", includeBiophysicalProperties=False)
+#export_to_neuroml2("load_bc6.hoc", "BC6.cell.nml", includeBiophysicalProperties=False)
+export_to_neuroml2("load_bc2.hoc", "BC2.cell.nml", includeBiophysicalProperties=False)
