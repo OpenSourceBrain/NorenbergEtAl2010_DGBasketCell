093b052d0653e99af1e81e07133cdc25db79628a
Padraig Gleeson <p.gleeson@gmail.com>
2017-03-09 19:23:01 +0000

    Identifying soma/axon/dend of segments & creating groups for cond density assignment
    CC @BKriener

--- NeuroML2/CreateCellModel.py
+++ NeuroML2/CreateCellModel.py
@@ -9,12 +9,47 @@ print("Loaded morphology file from: "+fn)
 
 cell = doc.cells[0]
 
+axon_seg_group = neuroml.SegmentGroup(id="axon_group",neuro_lex_id="GO:0030424")  # See http://amigo.geneontology.org/amigo/term/GO:0030424
+soma_seg_group = neuroml.SegmentGroup(id="soma_group",neuro_lex_id="GO:0043025")
+dend_seg_group = neuroml.SegmentGroup(id="dendrite_group",neuro_lex_id="GO:0030425")
+apic_dend_seg_group = neuroml.SegmentGroup(id="apic_dendrite_group")
+
+included_sections = []
+for seg in cell.morphology.segments:
+    neuron_section_name = seg.name[seg.name.index('_')+1:]
+    
+    if not neuron_section_name in included_sections:
+        if 'axon' in seg.name:
+            axon_seg_group.includes.append(neuroml.Include(segment_groups=neuron_section_name))
+        elif 'soma' in seg.name:
+            soma_seg_group.includes.append(neuroml.Include(segment_groups=neuron_section_name))
+        elif 'dend' in seg.name:
+            dend_seg_group.includes.append(neuroml.Include(segment_groups=neuron_section_name))
+        elif 'apic' in seg.name:
+            dend_seg_group.includes.append(neuroml.Include(segment_groups=neuron_section_name))
+            apic_dend_seg_group.includes.append(neuroml.Include(segment_groups=neuron_section_name))
+        else:
+            raise Exception("Segment: %s is not axon, dend or soma!"%seg)
+    
+    included_sections.append(neuron_section_name)
+
+cell.morphology.segment_groups.append(axon_seg_group)
+cell.morphology.segment_groups.append(soma_seg_group)
+cell.morphology.segment_groups.append(dend_seg_group)
+cell.morphology.segment_groups.append(apic_dend_seg_group)
+
 channel_densities = []
 
 cd_pas = neuroml.ChannelDensity(id="pas_chan", segment_groups="all", ion="non_specific", ion_channel="pas", erev="-70.0 mV", cond_density="0.021 mS_per_cm2")
 channel_densities.append(cd_pas)
 
-cd_na = neuroml.ChannelDensity(id="na_chan", segment_groups="all", ion="na", ion_channel="Na_BC", erev="55 mV", cond_density="50 mS_per_cm2")
+cd_na = neuroml.ChannelDensity(id="na_chan_soma", segment_groups=soma_seg_group.id, ion="na", ion_channel="Na_BC", erev="55 mV", cond_density="150 mS_per_cm2")
+channel_densities.append(cd_na)
+
+cd_na = neuroml.ChannelDensity(id="na_chan_dend", segment_groups=dend_seg_group.id, ion="na", ion_channel="Na_BC", erev="55 mV", cond_density="30 mS_per_cm2")
+channel_densities.append(cd_na)
+
+cd_na = neuroml.ChannelDensity(id="na_chan_axon", segment_groups=axon_seg_group.id, ion="na", ion_channel="Na_BC", erev="55 mV", cond_density="30 mS_per_cm2")
 channel_densities.append(cd_na)
 
 cd_k = neuroml.ChannelDensity(id="k_chan", segment_groups="all", ion="k", ion_channel="K_BC", erev="-90 mV", cond_density="30 mS_per_cm2")
